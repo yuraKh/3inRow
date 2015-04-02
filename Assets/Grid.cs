b995a76fd6cbabd79b24996fba5e3562aac3fa5d
Юрій <comp9@Mac-mini-comp9.local>
2015-04-02 15:27:00 +0300

    0001

---
+++ Assets/Grid.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Grid : MonoBehaviour {
+	public Color[] Col = new Color[5];
+	int k;
+	public GameObject Cube;
+	int yP = 0;
+	int zP = 0;
+	static public GameObject [,] cub = new GameObject[10,5];
+	// Use this for initialization
+	void Start () {
+
+		for (int q = 0; q < 10; q++) {
+			zP = 0;
+			for (int j = 0; j < 5; j++) {
+				k = Random.Range (0, 5);
+				GameObject Cub = (GameObject)Instantiate (Cube);
+				Cub.transform.position = new Vector3 (Cub.transform.position.x + zP, Cub.transform.position.y + yP, Cub.transform.position.z);
+				Cub.GetComponent<MeshRenderer> ().material.color = Col [k];
+				Cub.transform.name = "square" + string.Format ("{0:D2}", q) + " " + string.Format ("{0:D2}", j);
+				cub [q, j] = Cub;
+				zP++;
+			}
+			yP++;
+
+		}
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/JumpColor.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class JumpColor : MonoBehaviour
+{
+	GameObject one = null;
+	GameObject two = null;
+	  // Use this for initialization
+    void Start()
+    {
+	}
+   
+    // Update is called once per frame
+    void Update()
+    {
+		if (Input.GetMouseButtonDown (0)) {
+			Ray xRay = Camera.main.ScreenPointToRay (Input.mousePosition);
+			RaycastHit hit;
+			if (Physics.Raycast (xRay, out hit, 100f)) {
+				if (one == null) {
+					one = hit.collider.gameObject;
+					iTween.RotateTo (one, iTween.Hash("y", 180, "looptype", "time", 5, iTween.LoopType.loop));
+				} else {
+					two = hit.collider.gameObject;
+					iTween.RotateTo (two, iTween.Hash("y", 180, "looptype", "time", 5, iTween.LoopType.loop));
+					if (Vector3.Distance(one.transform.position, two.transform.position) == 1) {
+						iTween.MoveTo (one, iTween.Hash ("x", two.transform.position.x, "y", two.transform.position.y, "z", two.transform.position.z, "time", 2, "easetype", iTween.EaseType.easeOutBounce));
+						iTween.MoveTo (two, iTween.Hash ("x", one.transform.position.x, "y", one.transform.position.y, "z", one.transform.position.z, "time", 2, "easetype", iTween.EaseType.easeOutBounce));
+						print (one.transform.position + " + " + two.transform.position);
+						if (two.GetComponent<MeshRenderer>().material.color == Grid.cub[(int)one.transform.position.y, (int)one.transform.position.x + 1].GetComponent<MeshRenderer>().material.color){
+							Destroy(gameObject);
+							Destroy ( Grid.cub[(int)one.transform.position.y, (int)one.transform.position.x + 1]);
+							
+						}
+					}
+					one = null;
+					two = null;
+				}
+			}
+		}
+	}
+}
